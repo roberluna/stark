6ac7b1902347079a8ee111fe2a5fe2f1321153f6
rober <rcluna@uach.mx>
2021-10-13 08:02:59 -0600

    Se cambio el titulo a H2, y se agrego el nombre de la compania

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stark32";
+
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $conn->prepare("CALL PR_LISTAR_NAVES(0);");
+  $stmt->execute();
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $registros = $stmt->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="container">
+  <h1>INDUSTRIAS STARK</h1>
+
+  <h2>NAVES DISPONIBLES</h2>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nombre</th>
+        <th>Modelo</th>
+        <th>Capacidad</th>
+        <th>Estatus</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($registros as $registro) { ?>
+          <tr>
+            <td><?php echo $registro['idnaves']; ?></td>
+            <td><?php echo $registro['nombre']; ?></td>
+            <td><?php echo $registro['modelo']; ?></td>
+            <td><?php echo $registro['capacidad']; ?></td>
+            <td><?php echo $registro['estatus']; ?></td>
+            <td><a href="#">Editar</a<</td>
+          </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+
+</body>
+</html>
